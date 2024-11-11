c74019eab0a94afb820ecd334fbf1d4643b1363b
Logan <logan@lightscale.io>
2024-11-11 19:39:55 +0900

    feat: db 초기화 스크립트

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+create table if not exists brand
+(
+    id   bigint       not null generated always as identity,
+    name varchar(255) not null,
+
+    primary key (id)
+);
+
+create table if not exists category
+(
+    id   bigint       not null generated always as identity,
+    name varchar(255) not null,
+
+    primary key (id)
+);
+
+create table if not exists product
+(
+    id          bigint not null generated always as identity,
+    brand_id    bigint not null,
+    category_id bigint not null,
+    price       bigint not null,
+
+    primary key (id)
+);
